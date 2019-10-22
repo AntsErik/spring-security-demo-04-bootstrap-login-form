e2be6a8a060a41a93cd22d0b778ac791dfd21da0
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-22 15:52:40 +0300

    Initial push.

---
+++ src/main/java/ee/praktika/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,15 @@
+package ee.praktika.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping( "/showMyLoginPage" )
+    public String showMyLoginPage(){
+
+        return "plain-login"; //maps over to /WEB-INF/view/plain-login.jsp --> need to create the file
+    }
+
+}
